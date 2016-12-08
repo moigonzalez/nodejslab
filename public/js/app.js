05ff4964ed21ef317bb6bbe2d3277ba93d45bcfb
Moises Gonzalez <moigoncas@gmail.com>
2016-12-08 10:45:33 +0100

    Defers javascript to prevent undefined values

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
 (function () {
 	
 	var RENDERED_POSTS_EP = $('#rendered_posts_ep').val();
-	
+
 	$.ajax({
 		url: RENDERED_POSTS_EP,
 		success: function (data) {
-			appendBlogPosts(data);
+			setTimeout(function () {
+				appendBlogPosts(data);
+			}, 200);
 		},
 		error: function (req, status, err) {
 			console.log(req, status, err);
